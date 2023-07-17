c1c46eaae1eed57eea4565a57292309f6388f0b6
szbmrk <szmark16@gmail.com>
2023-07-17 09:44:57 +0200

    db example api

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import mysql from "mysql"
 
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ origin: true, credentials: true }))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -15,5 +15,27 @@ const db = mysql.createConnection({
     password: '',
     database: 'elearning_portal',
 });
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
 
-app.listen(process.env.PORT, () => console.log("Server started on port: 5000"))
+app.get('/api', (req, res) => {
+    res.json({ msg: 'Hello from the backend!' })
+})
+
+app.get('/teachers', (req, res) => {
+    db.query('SELECT * FROM Teacher', (err, results) => {
+        if (err) {
+            console.error('Error retrieving teachers:', err);
+            res.status(500).json({ error: 'Failed to retrieve teachers' });
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.listen(5000, () => console.log(`Server started on port: 5000`))
