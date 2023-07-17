cc006fb7899e2fd46ad129cf90d402b4ea0d589d
szbmrk <szmark16@gmail.com>
2023-07-17 12:39:11 +0200

    f

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -33,41 +33,57 @@ const LoginForm = (props) => {
             // Store token in local storage
             localStorage.setItem("token", response.data[0].student_id);
             localStorage.setItem("role", role);
-            navigate(`/${role}/about`);
+            if (role === 'student')
+                navigate(`/student/about`);
+            else
+                navigate(`/teacher/courses/add`);
         } catch (error) {
             console.error("Error logging in:", error);
             // Handle error state
         }
     };
 
+    const changeLoginPage = () => {
+        if (role === 'student') {
+            navigate('/login/teacher');
+        } else {
+            navigate('/login/student');
+        }
+    }
+
     return (
-        <div className="container">
-            <h1>{role}</h1>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={handleEmailChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+        <div className="flex">
+            <div className="container">
+                <h1>Login as:</h1>
+                <h2 style={{ marginTop: '-20px' }}>{role}</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            {role === "student" ? <button onClick={changeLoginPage} type="button">Switch to teacher login page</button> :
+                <button onClick={changeLoginPage} type="button">Switch to student login page</button>}
         </div>
+
     );
 };
 
