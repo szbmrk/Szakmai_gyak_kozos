830d099e408342621d51edda48fdfc2d727de947
szbmrk <szmark16@gmail.com>
2023-07-19 11:54:34 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import Navbar from "./components/Navbar";
@@ -10,45 +11,89 @@ import OwnCourses from "./components/teacher/OwnCourses";
 import AddCourse from "./components/teacher/AddCourse";
 import AddAssignmentForm from "./components/teacher/AddAssignmentForm";
 import OwnAssignments from "./components/teacher/OwnAssignments";
+import NoPermission from "./components/NoPermission";
 
 function App() {
+    const [currentRole, setCurrentRole] = useState(localStorage.getItem("role"));
+    useEffect(() => {
+        setCurrentRole(localStorage.getItem("role"));
+    }, []);
+
+    const checkPermission = (role, component) => {
+        if (currentRole === role) {
+            return component;
+        }
+        return <NoPermission />;
+    };
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="*" element={<Navbar />} />
             </Routes>
             <Routes>
-                <Route path="/student/about" element={<TeachersList />} />
+                <Route
+                    path="/student/about"
+                    element={checkPermission("student", <TeachersList />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/student/mycourses" element={<CoursesList />} />
+                <Route
+                    path="/student/mycourses"
+                    element={checkPermission("student", <CoursesList />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/student/assignments/:courseId" element={<AssignmentsList />} />
+                <Route
+                    path="/student/assignments/:courseId"
+                    element={checkPermission("student", <AssignmentsList />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/student/enroll" element={<AvailableCourses />} />
+                <Route
+                    path="/student/enroll"
+                    element={checkPermission("student", <AvailableCourses />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/teacher/mycourses" element={<OwnCourses />} />
+                <Route
+                    path="/teacher/mycourses"
+                    element={checkPermission("teacher", <OwnCourses />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/teacher/courses/add" element={<AddCourse />} />
+                <Route
+                    path="/teacher/courses/add"
+                    element={checkPermission("teacher", <AddCourse />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/teacher/assignments" element={<OwnAssignments />} />
+                <Route
+                    path="/teacher/assignments"
+                    element={checkPermission("teacher", <OwnAssignments />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/teacher/assignments/add" element={<AddAssignmentForm />} />
+                <Route
+                    path="/teacher/assignments/add"
+                    element={checkPermission("teacher", <AddAssignmentForm />)}
+                />
             </Routes>
             <Routes>
-                <Route path="/login/student" element={<LoginForm role="student" />} />
+                <Route
+                    path="/login/student"
+                    element={<LoginForm role="student" />}
+                />
             </Routes>
             <Routes>
-                <Route path="/login/teacher" element={<LoginForm role="teacher" />} />
+                <Route
+                    path="/login/teacher"
+                    element={<LoginForm role="teacher" />}
+                />
             </Routes>
         </BrowserRouter>
     );
 }
+
 
 export default App;
